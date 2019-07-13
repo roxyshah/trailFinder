53f7ddf7beecd8ab596089c2badd3e842ab142af
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-07-13 13:46:02 -0400

    feature addition - search by location

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const apiKey = '200518328-8bca963655d57f39a898f6a5ed451149'; 
+const apiKey = '200518328-8bca963655d57f39a898f6a5ed451149';
+const googleAPIKey = 'AIzaSyAeGiEt4c7marM2v1Z2EAM2yH4rjFcjvXo';
 const searchURL = 'https://www.hikingproject.com/data/get-trails';
+const googleSearchURL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -17,11 +21,37 @@ function displayResults(responseJson) {
         console.log(responseJson.trails[i].name);
         const listItem = '<li><a href="' + responseJson.trails[i].url + '" target="_blank">' 
         + responseJson.trails[i].name 
-        +'</a><p>' + responseJson.trails[i].location + '</p><p>' + responseJson.trails[i].summary + '</p><p>' + responseJson.trails[i].length + ' miles</p></li>';
+        +'</a><p>' + '<img src="' + responseJson.trails[i].imgMedium +'"/>' + '</p><p>' + responseJson.trails[i].location + '</p><p>' + responseJson.trails[i].summary + '</p><p>' + responseJson.trails[i].length + ' miles</p></li>';
         $('#results-list').append(listItem);
     }
     $('#results').removeClass('hidden');
 }
+
+function getLatLng (address, maxResults=10) {
+  const params = {
+    address: address,
+    key: googleAPIKey
+  }
+  const queryString = formatQueryParams(params);
+  const url = googleSearchURL + '?' + queryString;
+
+  // console.log(url);
+
+  return fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => {
+    console.log(responseJson.results[0].geometry.location);
+    const location = responseJson.results[0].geometry.location;
+    renderTrailList(location.lat, location.lng, maxResults);
+  });
+
+}
+
 function getTrailList(userLat, userLong, maxResults=10) {
   const params = {
     lat: userLat,
@@ -29,7 +59,7 @@ function getTrailList(userLat, userLong, maxResults=10) {
     maxResults: maxResults,
     key: apiKey
   };
-  const queryString = formatQueryParams(params)
+  const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
 
   console.log(url);
@@ -61,11 +91,9 @@ function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     $('#results-list').empty();
-    // const searchTerm = $('#searchNatlParks').val();
     const maxResults = $('#js-max-results').val();
-    const userLat = $('#userLat').val();
-    const userLong = $('#userLong').val();
-    renderTrailList(userLat, userLong, maxResults);
+    const address = $('#userAddress').val();
+    getLatLng(address, maxResults);
   });
 }
 
