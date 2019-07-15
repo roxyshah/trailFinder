d040239aacdd011e94b87c4c80e5ecb1bb7efef0
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-07-14 21:32:13 -0400

    improving site appearence

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ function displayResults(responseJson) {
     console.log(responseJson);
     for(let i = 0; i < responseJson.trails.length; i++) {
         console.log(responseJson.trails[i].name);
-        const listItem = '<li><p><a href="' + responseJson.trails[i].url + '" target="_blank">' 
-        + responseJson.trails[i].name 
-        +'</a></p><p>' + '<img src="' + responseJson.trails[i].imgMedium +'"/>' + '</p><p>' + responseJson.trails[i].location + '</p><p>' + responseJson.trails[i].summary + '</p><p>' + responseJson.trails[i].length + ' miles</p></li>';
+        const listItem = '<li><a href="' + responseJson.trails[i].url + '" target="_blank"><p class="linkName">' 
+        + responseJson.trails[i].name +'</p><p>' + '<img src="' + responseJson.trails[i].imgMedium +'"/>' + 
+        '</p></a><p class="summary">' + responseJson.trails[i].summary + '</p><p>' + responseJson.trails[i].location + 
+        '</p><p>' + responseJson.trails[i].length + ' miles</p></li>';
         $('#results-list').append(listItem);
     }
     $('#results').removeClass('hidden');
